e5d79e10499e870e6f2f61ecd57e50e413dca7bb
Andres Marin <andres.marin.brito@ingka.ikea.com>
2024-01-14 18:57:32 +0100

    Paint API Debugging

--- api/paint.js
+++ api/paint.js
@@ -1,11 +1,17 @@
 export default function handler(req, res) {
+    console.log("START")
+    console.log("REQ", req)
     const { command } = req.query
-    const responseUrl = req.headers.get('Nightbot-Response-Url')
-    const user = req.headers.get('Nightbot-User') ? new URLSearchParams(req.headers.get('Nightbot-User')) : null
-    const channel = req.headers.get('Nightbot-Channel') ? new URLSearchParams(req.headers.get('Nightbot-Channel')) : null
-
-    if(user){
-        //DO SOMETHING
+    try{
+        const responseUrl = req.headers.get('Nightbot-Response-Url')
+        const user = req.headers.get('Nightbot-User') ? new URLSearchParams(req.headers.get('Nightbot-User')) : null
+        const channel = req.headers.get('Nightbot-Channel') ? new URLSearchParams(req.headers.get('Nightbot-Channel')) : null
+    
+        if(user){
+            //DO SOMETHING
+        }
+    }catch(e){
+        console.log(e)
     }
 
     return res.json({
