d6f36b3b2cfd870d8019662d2f642760aaf4112e
Andres Marin <andres.marin.brito@ingka.ikea.com>
2024-01-15 11:56:41 +0100

    Clear API

--- api/clear.js
+++ api/clear.js
@@ -4,10 +4,10 @@ export default async function handler(req, res) {
     console.log("START", req.query)
     console.log("params", req.query)
     
-    const commands = await kv.lrange('RECENT_PAINT_COMMANDS', 0, -1)
-    await kv.del('RECENT_PAINT_COMMANDS')
+    const result = await kv.flushall()
+
     return res.json({response: {
-        commands: commands,
-        matrix: await kv.lrange('MATRIX', 0, -1),
+        ok: "true",
+        result: result
     }})
 }
